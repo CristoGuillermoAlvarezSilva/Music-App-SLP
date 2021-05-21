c3e7bd835160c8c132fe5b41965592f20ecff58b
CristoGuillermoAlvarezSilva <35870620+CristoGuillermoAlvarezSilva@users.noreply.github.com>
2021-05-21 11:58:08 -0500

    Eventos Index

--- MusicApp/MusicApp/resources/views/eventos/index.blade.php
+++ MusicApp/MusicApp/resources/views/eventos/index.blade.php
@@ -6,62 +6,204 @@
 
 <!--Contenido de la pagina-->
 @section('content')
-    <h1 class="text-center">Eventos</h1>
-    @guest
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-6">
+        <section id="hero" class="d-flex align-items-center justify-content-center ">
+            
+            <!-- ======= Contact Section ======= -->
+            <section id="contact" class="contact">
+            <div class="container" data-aos="fade-up">
 
-    @else
-        <a class="btn btn-dark" type="button" href="/eventos/create">
-            Agregar Evento
-        </a>
-    @endguest
+                <div class="section-title">
+                <h2>Galeria</h2>
+                <p class="text-white">Encuentra el genero musical que prefieras!</p>
+                </div>
+
+                
+
+            </div>
+            </section><!-- End Contact Section -->
+
+            </div>
+        </section>
+        <section id="services" class="services">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+                <p class="py-md-5"></p>
+                @guest
+                <h2>Disponibles </h2>
+                @else
+                    @if(Auth::user()->rol == "super" || Auth::user()->rol == "Administrador")
+                    <!--Administrador-->
+                <h2>SuperUsuario </h2>
+                
+                @endif
+                    @endguest
+            
+            <p>Generos</p>
+            @guest
+                @else
+                    @if(Auth::user()->rol == "super" || Auth::user()->rol == "Administrador")
+                    <!--Administrador-->
+                <div class="d-flex justify-content-end mb-2">
+                    <a href="/eventos/create" class="btn btn-warning">
+                        Agregar Evento musical
+                    <i class="fas fa-plus"></i>
+                    </a>
+                </div>
+                
+                @endif
+                    @endguest
+            </div>
+
+            <?php
+
+$cuenta = 0;
+
+?>
 
 
-    <!--Todos usuarios-->
-   <div class="categoria-items row py-5">
-
-                @foreach($eventos as $item)
-        
-                    <div class="categoria-item col-12 col-md-3 pt-1">
-                        <div class="car">
-                            <div class="fon">
-                                <img src="/{{$item->path}}" alt="" width="300px" height="250px">
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        <ul class="tipo justify-content-center">Banda: {{$item->nombre}}</ul>
-                                        <ul class="tipo justify-content-center">Titulo del evento:{{$item->titulo}}</ul>
-                                        <ul class="tipo justify-content-center">Descripcion: {{$item->descripcion}}</ul>
-                                        <ul class="tipo justify-content-center">Lugar: {{$item->lugar}}</ul>
-                                        <ul class="tipo justify-content-center">Fecha: {{$item->fecha}}</ul>
-                                        <ul class="tipo justify-content-center">Hora: {{$item->hora}}</ul>
-                                        <ul class="tipo justify-content-center">Costo: {{$item->costo}}</ul>
-                                    </div>
-                                </div>
-                               
-                                @guest
-                                    @else
-                                        @if(Auth::user()->id == $item->idR)
-                                            <div class="d-flex">
-                                                <a href="/eventos/{{$item->id}}/edit" class="btn btn-info">
-                                                    Editar
-                                                </a>
-                                            </div>
-                                            <form action="/eventos/{{$item->id}}" method="POST">
-                                                @csrf 
-                                                @method('DELETE')
-                                                <button class="btn btn-danger" type="submit">
-                                                Eliminar
-                                                </button>
-                                            </form>
-                                        @endif
-                                @endguest
-                            </div>
-                            
+        <div class="row">
+            <!------------------------------------------------------------------------------------------------------------------------------>
+            @foreach($eventos as $item)
+                @if($cuenta == 0)
+                
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="icon-box">
+                        <div ><img src="/{{$item->path}}" alt="Logo" class="img-fluid" width="300" ></i></div>
+                        <h4>{{$item->nombre}}</h4>
+                        
+                                        <p>Titulo del evento:{{$item->titulo}}</p>
+                                        <p>Descripcion: {{$item->descripcion}}</p>
+                                        <p>Lugar: {{$item->lugar}}</p>
+                                        <p>Fecha: {{$item->fecha}}</p>
+                                        <p>Hora: {{$item->hora}}</p>
+                                        <p>Costo: {{$item->costo}}</p>
+                        @guest
+                            @else
+                                @if(Auth::user()->rol == "super" || Auth::user()->rol == "Administrador")
+                                <p><a href="/eventos/{{$item->id}}/edit">Editar</a></p>
+                                <form action="/eventos/{{$item->id}}" method="POST">
+                                    @csrf 
+                                    @method('DELETE')
+                                        <button class="btn btn-danger" type="submit">Eliminar</button>
+                                </form>
+                                @endif
+                        @endguest
+                        
                         </div>
                     </div>
-                @endforeach   
-    </div>
-@endsection
+                   
+                @endif
+                @if($cuenta == 1)
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+                <div class="icon-box">
+                        <div ><img src="/{{$item->path}}" alt="Logo" class="img-fluid" width="300" ></i></div>
+                        <h4>{{$item->nombre}}</h4>
+                        
+                                        <p>Titulo del evento:{{$item->titulo}}</p>
+                                        <p>Descripcion: {{$item->descripcion}}</p>
+                                        <p>Lugar: {{$item->lugar}}</p>
+                                        <p>Fecha: {{$item->fecha}}</p>
+                                        <p>Hora: {{$item->hora}}</p>
+                                        <p>Costo: {{$item->costo}}</p>
+                        @guest
+                            @else
+                                @if(Auth::user()->rol == "super" || Auth::user()->rol == "Administrador")
+                                <p><a href="/eventos/{{$item->id}}/edit">Editar</a></p>
+                                <form action="/eventos/{{$item->id}}" method="POST">
+                                    @csrf 
+                                    @method('DELETE')
+                                        <button class="btn btn-danger" type="submit">Eliminar</button>
+                                </form>
+                                @endif
+                        @endguest
+                        
+                        </div>
+                </div>
+                @endif
+                @if($cuenta == 2)
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
+                <div class="icon-box">
+                        <div ><img src="/{{$item->path}}" alt="Logo" class="img-fluid" width="300" ></i></div>
+                        <h4>{{$item->nombre}}</h4>
+                        
+                                        <p>Titulo del evento:{{$item->titulo}}</p>
+                                        <p>Descripcion: {{$item->descripcion}}</p>
+                                        <p>Lugar: {{$item->lugar}}</p>
+                                        <p>Fecha: {{$item->fecha}}</p>
+                                        <p>Hora: {{$item->hora}}</p>
+                                        <p>Costo: {{$item->costo}}</p>
+                        @guest
+                            @else
+                                @if(Auth::user()->rol == "super" || Auth::user()->rol == "Administrador")
+                                <p><a href="/eventos/{{$item->id}}/edit">Editar</a></p>
+                                <form action="/eventos/{{$item->id}}" method="POST">
+                                    @csrf 
+                                    @method('DELETE')
+                                        <button class="btn btn-danger" type="submit">Eliminar</button>
+                                </form>
+                                @endif
+                        @endguest
+                        
+                        </div>
+                </div>
+                @endif
+                @if($cuenta >= 3)
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="300">
+                <div class="icon-box">
+                        <div ><img src="/{{$item->path}}" alt="Logo" class="img-fluid" width="300" ></i></div>
+                        <h4>{{$item->nombre}}</h4>
+                        
+                                        <p>Titulo del evento:{{$item->titulo}}</p>
+                                        <p>Descripcion: {{$item->descripcion}}</p>
+                                        <p>Lugar: {{$item->lugar}}</p>
+                                        <p>Fecha: {{$item->fecha}}</p>
+                                        <p>Hora: {{$item->hora}}</p>
+                                        <p>Costo: {{$item->costo}}</p>
+                        @guest
+                            @else
+                                @if(Auth::user()->rol == "super" || Auth::user()->rol == "Administrador")
+                                <p><a href="/eventos/{{$item->id}}/edit">Editar</a></p>
+                                <form action="/eventos/{{$item->id}}" method="POST">
+                                    @csrf 
+                                    @method('DELETE')
+                                        <button class="btn btn-danger" type="submit">Eliminar</button>
+                                </form>
+                                @endif
+                        @endguest
+                        
+                        </div>
+                </div>
+                @endif
+                
+                <?php
 
+$cuenta = $cuenta +1;
+
+?>
+            @endforeach
+        </div>
+
+
+
+            
+        </div>
+    </section>
+    
+    
+    
+    <!-- End Services Section -->
+    
+    </div>
+  </div>
+</div>
+
+
+
+@endsection
 
 
 
