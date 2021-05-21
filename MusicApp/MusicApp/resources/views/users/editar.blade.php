b17ecc1a824612a71076d4e221dcdcb7777b1e3e
CristoGuillermoAlvarezSilva <35870620+CristoGuillermoAlvarezSilva@users.noreply.github.com>
2021-05-21 12:01:43 -0500

    Users editar

--- MusicApp/MusicApp/resources/views/users/editar.blade.php
+++ MusicApp/MusicApp/resources/views/users/editar.blade.php
@@ -7,8 +7,46 @@
 
 
 @section('content')
-    <h3 class="py-4 text-center ">Editar Usuario</h3>
-    @if($error != "")
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-6">
+        <section id="hero" class="d-flex align-items-center justify-content-center ">
+            
+            <!-- ======= Contact Section ======= -->
+            <section id="contact" class="contact">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                <h2>Usuario</h2>
+                <p class="text-white">Cambia el rol de usuario!</p>
+                </div>
+
+                
+
+            </div>
+            </section><!-- End Contact Section -->
+
+            </div>
+        </section>
+
+    <section id="contact" class="contact">
+      <div class="container py-md-5" data-aos="fade-up">
+
+
+        <div class="row mt-5">
+
+                <div class="col-lg-2">
+                <div class="info">
+                    <div class="address">
+                    <img src="assets/img/group.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+            </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0 align-self-center py-md-5">
+
+          @if($error != "")
         <div class="alert alert-danger" role="alert">
             {{$error}}
         </div>
@@ -47,9 +85,21 @@
         @endguest
 
         <div class="col-12 text-center">
-            <button class="btn btn-success" type="submit">Actualizar</button>
+            <button class="btn btn-warning" type="submit">Registrar!</button>
         </div>
     </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    </div>
+  </div>
+</div>
+
 @endsection
+
 
  
