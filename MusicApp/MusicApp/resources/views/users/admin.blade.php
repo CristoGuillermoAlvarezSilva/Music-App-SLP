3523a4e334ec161abf8ec777dd6297469bdbb6c5
CristoGuillermoAlvarezSilva <35870620+CristoGuillermoAlvarezSilva@users.noreply.github.com>
2021-05-21 12:01:05 -0500

    Admin

--- MusicApp/MusicApp/resources/views/users/admin.blade.php
+++ MusicApp/MusicApp/resources/views/users/admin.blade.php
@@ -10,22 +10,48 @@
 @endsection
 
 @section('content')
-<div class="">
-    <h3 class="py-8 text-center">Panel</h3>
-    <div class="d-flex flex-column flex-md-row flex-md-wrap justify-content-center py-5">
-        <a class="btn btn-outline-dark m-2" style="font-size: 8em" href="/users">
-            <h5 class="text-center">Usuarios</h5>
-            <img src="{{asset('img/usuarios.png')}}" alt="Logo" class="img-fluid" width="125" >
-        </a>
-        <a class="btn btn-outline-dark m-2" style="font-size: 8em" href="/generos">
-            <h5 class="text-center">Generos</h5>
-            <img src="{{asset('img/generos.png')}}" alt="Logo" class="img-fluid" width="125" >
-        </a>
-        
-        
-    
-    </div>
-</div>
+
+<section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <p class="py-md-5"></p>
+            <p class="py-md-5"></p>
+          <h2>SuperUsuario </h2>
+          <p>Panel</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><img src="{{asset('img/usuarios.png')}}" alt="Logo" class="img-fluid" width="125" ></i></div>
+              <h4><a href="/users">Usuarios</a></h4>
+              <p>Administra los Usuarios registrados en la plataforma!</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-slideshow"></i></div>
+              <h4><a href="/generos">Generos</a></h4>
+              <p>Administra los Generos registrados en la plataforma!</p>
+            </div>
+          </div>
+
+
+        </div>
+        <p class="py-md-5"></p>
+            <p class="py-md-5"></p>
+            <p class="py-md-5"></p>
+            <p class="py-md-5"></p>
+            <p class="py-md-5"></p>
+            <p class="py-md-5"></p>
+      </div>
+    </section><!-- End Services Section -->
+
+
+
+
 
 @endsection
 
